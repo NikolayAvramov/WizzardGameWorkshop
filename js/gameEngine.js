ba1a666756e7bb5a9165603ce461a3b86f10f9d5
Nikolay <avramov94@abv.bg>
2022-10-29 12:21:02 +0300

    Movment restrction

--- js/gameEngine.js
+++ js/gameEngine.js
@@ -1,5 +1,9 @@
 document.addEventListener("keydown", onKeyDown);
 document.addEventListener("keyup", onKeyup);
+const gameAreas = document.querySelector(".game-area");
+const displayHeight = gameAreas.clientHeight;
+const displayWidth = gameAreas.clientWidth;
+console.log(displayHeight);
 function onKeyDown(e) {
 	keys[e.code] = true;
 	console.log(keys);
@@ -9,27 +13,31 @@ function onKeyup(e) {
 	console.log(keys);
 }
 function gameAction() {
+	let currentHeight = wizzardObj.posY;
+	let currentWidth = wizzardObj.posX;
 	const wizzardElement = document.getElementById("wizzard");
-	if (keys.KeyA) {
-		console.log("sdf");
+	if (keys.KeyA && currentWidth > 0) {
 		wizzardObj.posX -= wizzardObj.speed;
 		wizzardElement.style.left = wizzardObj.posX + "px";
 	}
-	if (keys.KeyS) {
+	if (keys.KeyS && currentHeight + wizzardObj.height < displayHeight) {
 		wizzardObj.posY += wizzardObj.speed;
 		wizzardElement.style.top = wizzardObj.posY + "px";
 	}
-	if (keys.KeyD) {
+	if (keys.KeyD && currentWidth + wizzardObj.width < displayWidth) {
 		wizzardObj.posX += wizzardObj.speed;
 		wizzardElement.style.left = wizzardObj.posX + "px";
 	}
-	if (keys.KeyW) {
+	if (keys.KeyW && currentHeight > 0) {
 		wizzardObj.posY -= wizzardObj.speed;
 		wizzardElement.style.top = wizzardObj.posY + "px";
 	}
 	if (keys.Space) {
 		wizzardElement.classList.add("wizzard-fire");
 		wizzardElement.classList.remove("wizzard");
+	} else {
+		wizzardElement.classList.remove("wizzard-fire");
+		wizzardElement.classList.add("wizzard");
 	}
 	document.addEventListener("keypress", onKeyDown);
 	window.requestAnimationFrame(gameAction);
--- js/objectGame.js
+++ js/objectGame.js
@@ -1,8 +1,6 @@
-// let randomHeihght = Math.floor(
-// 	Math.random() * gameArea.clientHeight - wizzardElement.height
-// );
-// console.log(randomHeihght);
 let wizzardObj = {
+	width: 82,
+	height: 100,
 	posY: 400,
 	posX: 400,
 	speed: 5,
